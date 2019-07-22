e285c83a39fd95ad033694c1683a2859f7e10f12
an-hyojin <ahj1848@naver.com>
2019-07-22 20:46:17 +0900

    갤러리 권한 추가, drawActivity생성

---
+++ app/src/main/java/com/example/practice/DrawActivity.java
@@ -0,0 +1,47 @@
+package com.example.practice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class DrawActivity extends AppCompatActivity implements View.OnClickListener {
+    private final int GALLERY_CODE=1112;
+    Button galleryBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+        galleryBtn = (Button) findViewById(R.id.gallary);
+        galleryBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.gallary:
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_CODE);
+
+
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case GALLERY_CODE:
+
+                    break;
+                default:
+                    break;
+             }
+        }
+    }
+}
--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -8,22 +8,38 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button button1, button2, button3,button4,button5,button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.btn1);
+        button1.setOnClickListener(this);
+        button2 = (Button)findViewById(R.id.btn2);
+        button2.setOnClickListener(this);
 
-        Button button3 = (Button)findViewById(R.id.btn3);
-
+        button3 = (Button)findViewById(R.id.btn3);
         button3.setOnClickListener(this);
+        button4 = (Button)findViewById(R.id.btn4);
+        button4.setOnClickListener(this);
+        button5 = (Button)findViewById(R.id.btn5);
+        button5.setOnClickListener(this);
+        button6 = (Button)findViewById(R.id.btn6);
+        button6.setOnClickListener(this);
 
     }
 
     public void onClick(View v){
+        Intent intent;
         switch (v.getId()){
-            case R.id.btn3:
-                Intent intent = new Intent(this, CardActivity.class);
+            case R.id.btn1:
+                intent = new Intent(this, DrawActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.btn3:
+                intent = new Intent(this, CardActivity.class);
+                startActivity(intent);
+                break;
         }
     }
 }
