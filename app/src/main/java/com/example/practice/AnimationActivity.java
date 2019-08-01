79357106e82c2962a98c8f0206835b59b0476c4b
201502029 <tnehd158@gmail.com>
2019-08-02 03:30:18 +0900

    유튜브 api 이용부분을 주석처리하고 실행하니 정상실행됨,, 아직 미완성

--- app/src/main/java/com/example/practice/AnimationActivity.java
+++ app/src/main/java/com/example/practice/AnimationActivity.java
@@ -12,10 +12,10 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class AnimationActivity extends AppCompatActivity implements View.OnClickListener {
 
-    YouTubePlayerView youtubeView;
+//    YouTubePlayerView youtubeView;
     ImageButton backBtn;
-    Button button;
-    YouTubePlayer.OnInitializedListener listener;
+//    Button button;
+//    YouTubePlayer.OnInitializedListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +23,27 @@ public class AnimationActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_animation);
         backBtn = (ImageButton)findViewById(R.id.back);
         backBtn.setOnClickListener(this);
-        button = (Button) findViewById(R.id.youtubeButton);
-        youtubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
-        listener = new YouTubePlayer.OnInitializedListener() {
-
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("hJorGesHoFQ");
-            }//유튜브 로드 성공했을 때
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
-            }//유튜브 로드 실패했을 때
-        };
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                youtubeView.initialize("AIzaSyDjVs7eH1IZAxpoEjufARWr_sMrM8KbV98", listener);
-            }
-        });
+//        button = (Button) findViewById(R.id.youtubeButton);
+//        youtubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+//        listener = new YouTubePlayer.OnInitializedListener() {
+//
+//            @Override
+//            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+//                youTubePlayer.loadVideo("hJorGesHoFQ");
+//            }//유튜브 로드 성공했을 때
+//
+//            @Override
+//            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+//                onBackPressed();
+//            }//유튜브 로드 실패했을 때
+//        };
+//        button.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                youtubeView.initialize("AIzaSyDjVs7eH1IZAxpoEjufARWr_sMrM8KbV98", listener);
+//            }
+//        });
     }
 
     @Override
@@ -51,8 +51,6 @@ public class AnimationActivity extends AppCompatActivity implements View.OnClick
          switch (v.getId()){
             case R.id.back:
                 onBackPressed();
-
         }
     }
-
 }
